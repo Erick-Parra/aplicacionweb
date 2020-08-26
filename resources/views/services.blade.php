f0d2cc523d56d7fa6245dc2f5ca020692a2e9d83
Turcios <62146462+Turcios@users.noreply.github.com>
2020-08-26 00:29:04 -0600

    update service.blade.php
    Cambio descripcion

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -8,32 +8,43 @@
             </div>
             <div class="row features">
                 <div class="col-sm-6 col-md-4 item"><i class="fa fa-thermometer-half icon" id="icon-services-1"></i>
-                    <h3 class="name">control operativo</h3>
-                    <p class="description"><br>FALTA COMPLETAR<br><br></p>
+                    <h3 class="name">CONTROL OPERATIVO</h3>
+                    <p class="description"><br>Este servicio se realiza basado en el reglamento de calidad de agua potable (RCA) 38924-S,que establece el control operativo como un análisis de calidad de agua básico para conocer la situación operacional<br><br></p>
                 </div>
                 <div class="col-sm-6 col-md-4 item"><i class="fa fa-flask icon" id="icon-services"></i>
-                    <h3 class="name">TOMA DE MUESTRAS</h3>
-                    <p class="description"><br>FALTA COMPLETAR<br><br></p>
+                    <h3 class="name">ANÁLISIS DE CALIDAD DE AGUA</h3>
+                    <p class="description"><br>Los análisis de laboratorio son parte del reglamento 38924-S, 
+                    este servicio se subcontrata con el laboratorio TecnoLab y la Liga Comunal del Agua realiza el muestreo con todos los protocolos establecidos por el laboratorio.
+                    Dentro de los Análisis realizados por parte del laboratorio se tiene Análisis N1, 
+                    este análisis determina principalmente la presencia de Coliiformes fecales y  E. coli y se debe realizar semestralmente,el análisis N2 lo que determina principalmente es los minerales presentes en el agua y se debe realizar cada tres años y el análisis N3 que analiza los principales metales pesados y se debe realizar igualmente cada 3 años<br><br></p>
                 </div>
                 <div class="col-sm-6 col-md-4 item"><i class="fa fa-list-alt icon"></i>
-                    <h3 class="name">CONTABILIDAD</h3>
-                    <p class="description"><br>FALTA COMPLETAR<br><br></p>
+                    <h3 class="name">VENTAS DE EQUIPOS E INSUMOS</h3>
+                    <p class="description"><br>La LCA maneja dos líneas de cloradores cámara seca y bomba de inyección, esto con el fin de adaptarse a la necesidad de cada subsistema y garantizar la eficiencia y homogeneidad del cloro residual libre de la red de distribución, cumpliendo con uno de los parámetros indispensables en temas de calidad de agua<br><br></p>
                 </div>
                 <div class="col-sm-6 col-md-4 item"><i class="far fa-list-alt icon"></i>
                     <h3 class="name">FACTURACIÓN ELECTRÓNICA</h3>
-                    <p class="description"><br>FALTA COMPLETAR<br><br></p>
+                    <p class="description"><br>Considerando que muchas de las asadas del territorio no tienen acceso a internet ni cuentan con oficina, la LCA diseño este servicio con el fin de brindar la posibilidad de cumplir con este parámetro exigido por el AYA y Ministerio Hacienda<br><br></p>
                 </div>
-                <div class="col-sm-6 col-md-4 item"><i class="fa fa-legal icon"></i>
-                    <h3 class="name">ASESORÍA LEGAL</h3>
-                    <p class="description"><br>FALTA COMPLETAR<br><br></p>
+                    <div class="col-sm-6 col-md-4 item"><i class="fa fa-legal icon"></i>
+                    <h3 class="name">CONTABILIDAD DE ASADAS</h3>
+                    <p class="description"><br>El servicio de Contabilidad que realiza la LCA es basado en el manual de contabilidades establecido por el AYA,además la contadora encargada, trabaja exclusivamente con ASADAS,
+                      permitiéndonos tener más experiencia en el área y cumplido con los entes rectores<br><br></p>
                 </div>
                 <div class="col-sm-6 col-md-4 offset-lg-0 item"><i class="fas fa-hands-helping icon"></i>
-                    <h3 class="name">APOYO A ORGANIZACIONES</h3>
-                    <p class="description"><br>FALTA COMPLETAR<br><br></p>
+                    <h3 class="name">VENTA E INSTALACIÓN DE CLORADORES</h3>
+                    <p class="description"><br>La LCA maneja dos líneas de cloradores cámara seca y bomba de inyección,
+                     esto con el fin de adaptarse a la necesidad de cada subsistema y garantizar la eficiencia y homogeneidad del cloro residual libre de la red de distribución, 
+                     cumpliendo con uno de los parámetros indispensables en temas de calidad de agua<br><br></p>
                 </div>
                 <div class="col-sm-6 col-md-4 offset-lg-4 item"><i class="icon ion-person-stalker icon"></i>
-                    <h3 class="name">COMUNICACIÓN</h3>
-                    <p class="description"><br>FALTA COMPLETAR<br><br></p>
+                    <h3 class="name">LEVANTAMIENTO DE INFORMACIÓN GEOESPACIAL Y MAPEO</h3>
+                    <p class="description"><br>Este servicio consiste en el levantamiento GPS para la elaboración de croquis de campo de los acueductos comunales,identificando todos los subsistemas, características y complementarios con su debida georreferenciación<br><br></p>
+                </div>
+                <div class="col-sm-6 col-md-4 offset-lg-4 item"><i class="icon ion-person-stalker icon"></i>
+                    <h3 class="name">INVENTARIO DE ACTIVOS DE LA ASADA</h3>
+                    <p class="description"><br>Como parte de los labores para tener una contabilidad ordenada y en regla con base en el manual de cuentas del AYA,se debe inventariar y dar valor a todo el acueducto, 
+                    por esta razón la LCA brinda este servicio con personal capacitado y certificado para la labor, brindando a las ASADAS del territorio soluciones económicas y eficientes<br><br></p>
                 </div>
             </div>
         </div>
