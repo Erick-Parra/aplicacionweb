fb2e54a83e90b3e86a7a44e586a002daec933eb8
Priscila Villarreal <62452087+PriscilaVillarreal@users.noreply.github.com>
2020-08-28 01:49:27 -0600

    Modificación en información
    Se hicieron modificaciones en el cuadro de las ASADAS

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -4,125 +4,241 @@
 <html>
 <head>
 	<title></title>
+<title>Información LCA</title>
+
 </head>
 <style type="text/css">
-	table, th, td{
-		border: 1px solid blue;
+
+	table, th, td {
+		border: 6px solid DODGERBLUE;
 		border-collapse: collapse;
+		color: MIDNIGHTBLUE; 
+		background-color: SKYBLUE;
 	}
 	th, td{
-		padding: 10px;
+		padding: 15px;
+
 	}
 </style>
 <body>
+	<br>
+	<br>
+	<div style="text-align:center; background-color: PALETURQUOISE;">
+    <br>
 	<h1>ASADAS DEL ÁREA DE INFLUENCIA DE LA LCA</h1>
-	<table>
-		<tr>
-			<th> Cantón </th>
-			<th> # Abonados</th>
-			<th> # Usuarios Agua</th>
-			<th> # ASADAS Cantón</th>
-			<th> % ASADAS/Cantón</th>
-			
-		</tr>
-		<tr>
-			<td> Hojancha </td>
-			<td> 1,359 </td>
-			<td> 5,053 </td>
-			<td> 14 </td>
-			<td> 12 </td>
-		</tr>
-		<tr>
-			<td> Nandayure </td>
-			<td> 2,682 </td>
-			<td> 9,919 </td>
-            <td> 32 </td>
-            <td> 27 </td>
-		</tr>
-		<tr>
-			<td> Nicoya </td>
-			<td> 10,581 </td>
-			<td> 39,141 </td>
-			<td> 72 </td>
-			<td> 61 </td>
-		</tr>
-		<tr>
-			<td> Total</td>
-			<td> 14,622 </td>
-			<td> 54,113</td>
-			<td> 118</td>
-			<td> 100</td>
-		</tr>
-	</table>
-	<br>
-	<br>
-	<br>
+    <br>
+	<div style="text-align:center;">
+	<table class= "table table-hover table-dark" style="opacity: 1; border="5" style="margin: 0 auto;">
+		<caption>Lista de ASADAS del área de influencia de la LCA</caption>
+		<thead class="thead-dark">
+  <tr>
 
-	<h1>ASADAS DEL ÁREA DE INFLUENCIA DE LA LCA (Abonados)</h1>
-	<table>
-		<tr>
-			<th> Rango </th>
-			<th colspan="2"> Hojancha </th>
-			<th colspan="2"> Nadayure </th>
-			<th colspan="2"> Nicoya </th>
-			<th> Total </th>
-			<th> Porcentaje </th>
-		</tr>
-		<tr>
-			<td> < a 50 abonados </td>
-			<td> 05 </td>
-			<td> 36 </td>
-			<td> 08 </td>
-			<td> 25 </td>
-			<td> 15 </td>
-			<td> 21 </td>
-			<td> 28 </td>
-			<td> 25 </td>
-		</tr>
-		<tr>
-			<td> 51-150 abonados </td>
-			<td> 04 </td>
-			<td> 28 </td>
-			<td> 21 </td>
-			<td> 66 </td>
-			<td> 38 </td>
-			<td> 53 </td>
-			<td> 63 </td>
-			<td> 53 </td>
-		</tr>
-		<tr>
-			<td> 151-300 abonados </td>
-			<td> 05 </td>
-			<td> 36 </td>
-			<td> 01 </td>
-			<td> 03 </td>
-			<td> 11 </td>
-			<td> 15 </td>
-			<td> 17 </td>
-			<td> 14 </td>
-		</tr>
-		<tr>
-			<td> 301-700 abonados </td>
-			<td> 0 </td>
-			<td> 0 </td>
-			<td> 02 </td>
-			<td> 06 </td>
-			<td> 08 </td>
-			<td> 11 </td>
-			<td> 10 </td>
-			<td> 09 </td>
-		</tr>
-		<tr>
-			<td> Total </td>
-			<td> 14 </td>
-			<td> 100 </td>
-			<td> 32 </td>
-			<td> 100 </td>
-			<td> 72 </td>
-			<td> 100 </td>
-			<td> 118 </td>
-			<td> 100 </td>
-		</tr>
+    <th>CANTÓN</th>
+
+    <th># ABONADOS</th>
+
+    <th># USUARIOS AGUA</th>
+
+    <th># ASADAS CANTÓN</th>
+
+    <th>% ASADAS/CANTÓN</th>
+
+  </tr>
+
+  <tr>
+
+    <th>Hojancha</th>
+
+    <td>1,359</td>
+
+    <td>5,053</td>
+
+    <td>14</td>
+
+    <td>12%</td>
+
+  </tr>
+
+  <tr>
+
+    <th>Nandayure</th>
+
+    <td>2,682</td>
+
+    <td>9,919</td>
+
+    <td>32</td>
+
+    <td>27%</td>
+
+  </tr>
+
+  <tr>
+
+    <th>Nicoya</th>
+
+    <td>10,581</td>
+
+    <td>39,141</td>
+
+    <td>72</td>
+
+    <td>61%</td>
+
+  </tr>
+
+   <tr>
+
+    <th>Total</th>
+
+    <td>14,622</td>
+
+    <td>54,113</td>
+
+    <td>118</td>
+
+    <td>100%</td>
+
+  </tr>
+ 
+</table>
+    <br>
+	<br>
+	<div style="text-align:center; background-color: PALETURQUOISE;">
+    <br>
+   
+	<h1>ASADAS DEL ÁREA DE INFLUENCIA DE LA LCA</h1>
+    <br>
+	<div style="text-align:center;">
+   
+	<table class= "table table-hover table-dark" style="opacity: 1; border="5" title="center" style="margin: 0 auto;">
+		<caption>Lista de ASADAS del área de influencia de la LCA</caption>
+		<thead class="thead-dark">
+
+		 <th>Rango</th>
+
+    <th colspan="2">Hojancha</th>
+
+    <th colspan="2">Nandayure</th>
+
+    <th colspan="2">Nicoya</th>
+
+    <th>Total</th>
+
+    <th>Porcentaje</th>
+
+  </tr>
+
+  <tr>
+
+    <th>< a 50 abonados</th>
+
+    <td>05</td>
+
+    <td>36%</td>
+
+    <td>08</td>
+
+    <td>25%</td>
+
+    <td>15</td>
+
+    <td>21%</td>
+
+    <td>28</td>
+
+    <td>24%</td>
+
+  </tr>
+
+  <tr>
+
+    <th>51-150 abonados</th>
+
+    <td>04</td>
+
+    <td>28%</td>
+
+    <td>21</td>
+
+    <td>66%</td>
+
+    <td>38</td>
+
+    <td>53%</td>
+
+    <td>63</td>
+
+    <td>53%</td>
+
+  </tr>
+
+  <tr>
+
+    <th>150-300 abonados</th>
+
+    <td>05</td>
+
+    <td>36%</td>
+
+    <td>01</td>
+
+    <td>03%</td>
+
+    <td>11</td>
+
+    <td>15%</td>
+
+    <td>17</td>
+
+    <td>14%</td>
+
+  </tr>
+
+   <tr>
+
+    <th>301-700 abonados</th>
+
+    <td>0</td>
+
+    <td>0%</td>
+
+    <td>02</td>
+
+    <td>06%</td>
+
+    <td>08</td>
+
+    <td>11%</td>
+
+    <td>10</td>
+
+    <td>09%</td>
+
+  </tr>
+
+  <tr>
+
+    <th>Total</th>
+
+    <td>14</td>
+
+    <td>100%</td>
+
+    <td>32</td>
+
+    <td>100%</td>
+
+    <td>72</td>
+
+    <td>100%</td>
+
+    <td>118</td>
+
+    <td>100%</td>
+
+  </tr>
+
 	</table>
 
 <br>
@@ -130,5 +246,4 @@
 <br>
 
 </body>
-
 </html>
