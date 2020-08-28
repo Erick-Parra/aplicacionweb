31983b8d4a496852c73ab87f06639031be52dd26
Nancy-boop <62146590+Nancy-boop@users.noreply.github.com>
2020-08-27 20:10:30 -0600

    Revert "Prueba menu"
    This reverts commit befd47ddf7232a1f15464f43b1222cb6755286a5.

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/','home')->name('home');
 Route::view('/quienes-somos','who')->name('who');
-
 Route::view('/servicios','services')->name('services');
-Route::view('/Administrativos','services')-> name ('services');
-Route::view('/Técnicos','services')-> name ('services');
-Route::view('/Legales','services')-> name ('services');
-
 Route::view('/informacion','info')->name('info');
 Route::view('/contactenos','contact')->name('contact');
 
