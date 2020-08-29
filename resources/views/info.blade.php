ab3fc95c53c302d168dd3e990f770a431158e06e
Turcios <62146462+Turcios@users.noreply.github.com>
2020-08-29 00:17:41 -0600

    Commit info cuadro2
    En el segundo cuadro en el encabezado de los lugares, se agrego cantidad y %

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -115,13 +115,15 @@
 		<caption>Lista de ASADAS del área de influencia de la LCA</caption>
 		<thead class="thead-dark">
 
-		 <th>Rango Por Abonados</th>
+		<th>Rango Por Abonados</th>
 
-    <th colspan="2">Hojancha</th>
+    <th colspan="2"><p>  Hojancha</p> <br>
+    Cantidad &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Procentaje</th>
+    <th colspan="2"><p> Nandayure</p> <br>
+    Cantidad &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Procentaje</th>
 
-    <th colspan="2">Nandayure</th>
-
-    <th colspan="2">Nicoya</th>
+    <th colspan="2"><p>Nicoya</p> <br>
+    Cantidad &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Procentaje</th>
 
     <th>Total</th>
 
@@ -131,10 +133,10 @@
 
   <tr>
 
-    <th>< a 50</th>
+    <th>> a 50</th>
 
     <td>05</td>
-
+    
     <td>36%</td>
 
     <td>08</td>
