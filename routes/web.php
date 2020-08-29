013b2c8638a95bf0bb82d8133c3f769e75e01555
Nancy-boop <62146590+Nancy-boop@users.noreply.github.com>
2020-08-28 23:07:19 -0600

    Proceso Servicios

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/','inicio')->name('inicio');
 Route::view('/quienes-somos','who')->name('who');
-Route::view('/servicios','services')->name('services');
+
+Route::view('/serviciosAdmi','services')->name('services');
+Route::view('/serviciosTecn','servicesTecn')->name('servicesTecn');
+Route::view('/serviciosLegal','servicesLegal')->name('servicesLegal');
+
 Route::view('/informacion','info')->name('info');
 Route::view('/contactenos','contact')->name('contact');
 
