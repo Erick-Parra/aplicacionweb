4748276b7319225dcf52036b1eea241668cc8574
Parra2622 <erick.parra.matarrita@est.una.ac.cr>
2020-09-08 20:30:54 -0600

    Modelo Posts
    Para las noticias

---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Post
+ * 
+ * @property int $Id
+ * @property string $TituloNoti
+ * @property string $InfoNoti
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @package App\Models
+ */
+class Post extends Model
+{
+	protected $table = 'posts';
+	protected $primaryKey = 'Id';
+
+	protected $fillable = [
+		'TituloNoti',
+		'InfoNoti'
+	];
+}
