db4f4d990f60324501f49a7c4a6334b7d66f4bd3
Priscila Villarreal <62452087+PriscilaVillarreal@users.noreply.github.com>
2020-10-05 23:24:19 -0600

    Update contact.blade.php
    El formulario de contáctenos ya envía el correo. Se está utilizando Mailtrap para hacer pruebas durante el desarrollo del proyecto.

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\MessageReceived;
+
+class MessagesController extends Controller
+{
+    public function store()
+    {
+    	$message = request()->validate([ //Valida los datos del email 
+    		'firstname'=>'required',
+    		'lastname'=>'required',
+    		'phonenumber'=>'required',
+    		'email'=>'required',
+    		'asunto'=>'required',
+    		'messages'=>'required']);
+
+    	//Hace que se envíe el email a la dirección establecida
+    	Mail::to('priscila.villarreal.1306@gmail.com')->queue(new MessageReceived($message));
+
+    	//return new MessageReceived($message);
+    	//return 'Mensaje enviado';
+        session()->flash('alert-success', 'Enviado exitosamente, contestaremos en cuanto sea posible!'); 
+        return back();
+    	
+    }
+}
+
