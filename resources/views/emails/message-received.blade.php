d50abc7c7344a3da6a0e34e6460d91c1bef3d337
Priscila Villarreal <62452087+PriscilaVillarreal@users.noreply.github.com>
2021-04-15 22:31:02 -0600

    Formulario Contáctenos
    Se cambió para que los correos lleguen a Gmail, utilizando sendgrid.

--- resources/views/emails/message-received.blade.php
+++ resources/views/emails/message-received.blade.php
@@ -6,6 +6,8 @@
 <body>
 	<p>Recibiste un mensaje de: {{ $msg['firstname'] }} {{ $msg['lastname']}} - {{ $msg['email'] }}</p>
 	<p><strong>ASADA a la que pertenece:</strong> {{ $msg['nameasada']}}</p>
+	<p><strong>Número de teléfono:</strong> {{ $msg['phonenumber']}}</p>
+
 	<p><strong>Asunto:</strong> {{ $msg['asunto']}}</p>
 	<p><strong>Contenido:</strong> {{ $msg['messages']}}</p>
 </body>
